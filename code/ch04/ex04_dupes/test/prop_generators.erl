f8c42714c5605fe1ed145d7d24809b6673526452
Anatolii Kosorukov <java1cprog@yandex.ru>
2021-06-19 09:19:51 +0300

    Add more logging functionality

--- code/ch04/ex04_dupes/test/prop_generators.erl
+++ code/ch04/ex04_dupes/test/prop_generators.erl
@@ -6,27 +6,31 @@
 %%% Properties %%%
 %%%%%%%%%%%%%%%%%%
 prop_dupes() ->
- ?FORALL(KV, list({key(), val()}),
-        begin
-     M = maps:from_list(KV),
-     [maps:get(K, M) || {K, _V} <- KV], % crash if K's not in map
-     collect(
-       {dupes, to_range(5, length(KV) - length(lists:ukeysort(1,KV)))},
-       true
-     )
-  end).
-
+    ?FORALL(KV, (list({key(), val()})),
+            begin
+                M = maps:from_list(KV),
+				% if you would like to see 
+				% what {key,value} contains,
+				% please, uncommet next log block
+				% io:format("~n---------~n"),
+				% lists:foreach(fun({K, V}) ->
+				%                io:format("{Key = ~p, Value = ~p}",[K,V]) end, KV),
+				%io:format("~n---------~n"),				
+                _ = [maps:get(K, M) || {K, _V} <- KV], % crash if K's not in map
+                collect({dupes, to_range(5, length(KV) - length(lists:ukeysort(1, KV)))}, true)
+            end).
 
 %%%%%%%%%%%%%%%
 %%% Helpers %%%
 %%%%%%%%%%%%%%%
 key() -> integer().
+
 val() -> term().
 
 to_range(M, N) ->
     Base = N div M,
-    io:format("~2B = ~2B div ~B | {~2B, ~2B} = {~2B * ~B, (~B + 1)*~B~n",
-              [Base, N, M, Base * M, (Base + 1) * M, Base, M, Base, M]),
+    io:format("~2B = ~2B div ~B | {~2B, ~2B} = {~2B * ~B, (~B + 1)*~B}~n", 
+	          [Base, N, M, Base * M, (Base + 1) * M, Base, M, Base, M]),
     {Base * M, (Base + 1) * M}.
 
 %%%%%%%%%%%%%%%%%%
