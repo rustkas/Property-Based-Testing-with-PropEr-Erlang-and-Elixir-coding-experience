187753d65cdf0b0e09a5ec34d2654576f3d2e268
Anatolii Kosorukov <java1cprog@yandex.ru>
2021-06-20 11:34:51 +0300

    Add exercise 12

---
+++ code/ch04/ex12_queue/README.md
@@ -0,0 +1,34 @@
+An OTP library
+
+
+Create new OTP lib
+-----
+    $ rebar3 new lib ex12_queue && ex12_queue
+
+Add information to rebar.config
+
+
+Get dependencies
+-----
+    $ rebar3 get-deps
+
+
+Get PropEr help
+-----
+    $ rebar3 help proper
+
+
+Create new propety via PropEr
+-----
+    $ rebar3 new proper generators
+
+
+Format
+-----
+    $ rebar3 format
+
+
+PropEr test
+-----
+    $ rebar3 proper
+	
---
+++ code/ch04/ex12_queue/test/prop_generators.erl
@@ -0,0 +1,23 @@
+-module(prop_generators).
+
+-include_lib("proper/include/proper.hrl").
+
+%%%%%%%%%%%%%%%%%%
+%%% Properties %%%
+%%%%%%%%%%%%%%%%%%
+prop_queue_naive() ->
+    ?FORALL(List,
+            list({term(), term()}),
+            begin
+                Queue = queue:from_list(List),
+                queue:is_queue(Queue)
+            end).
+
+prop_queue_nicer() ->
+    ?FORALL(Q, queue(), queue:is_queue(Q)).
+
+%%%%%%%%%%%%%%%
+%%% Generators %%%
+%%%%%%%%%%%%%%%
+queue() ->
+    ?LET(List, list({term(), term()}), queue:from_list(List)).
