d602d1bf2f97f2ae0aa56357a00ef7945043b7ec
Anatolii Kosorukov <java1cprog@yandex.ru>
2021-06-21 22:10:22 +0300

    Add more examples

--- code/ch04/ex16_characters/README.md
+++ code/ch04/ex16_characters/README.md
@@ -33,3 +33,6 @@ PropEr test
     $ rebar3 proper
 	$ rebar3 proper -p prop_latin1_string --constraint_tries 80
 	$ rebar3 proper -p prop_latin2_string2
+	$ rebar3 proper -p prop_unicode_string --constraint_tries 2000
+	$ rebar3 proper -p prop_unicode_string2
+	$ rebar3 proper -p prop_unicode_string3
--- code/ch04/ex16_characters/test/prop_generators.erl
+++ code/ch04/ex16_characters/test/prop_generators.erl
@@ -15,6 +15,38 @@ prop_latin1_string2() ->
             is_list(String)
         ).
 
+prop_unicode_string() ->
+    ?FORALL(String, unicode_string(),
+			begin
+            io:format("~ts~n",[String]),
+			is_list(String)
+			end
+        ).
+		
+prop_unicode_string2() ->
+    ?FORALL(String, unicode_string2(),
+			begin
+            io:format("~ts~n",[String]),
+			is_bitstring(String)
+			end
+        ).		
+
+prop_unicode_string3() ->
+    ?FORALL(String, unicode_string3(),
+			begin
+            io:format("~ts~n",[String]),
+			is_list(String)
+			end
+        ).		
+
+prop_unicode_string4() ->
+    ?FORALL(String, unicode_string4(),
+			begin
+            io:format("~ts~n",[String]),
+			is_list(String)
+			end
+        ).
+		
 %%%%%%%%%%%%%%%
 %%% Helpers %%%
 %%%%%%%%%%%%%%%
@@ -30,5 +62,10 @@ latin1_string2() ->
   ?SUCHTHAT(S, ?SIZED(Size,vector(Size,integer(33, 126))), io_lib:printable_latin1_list(S)).
 
 unicode_string() ->
- ?SUCHTHAT(S, string(), io_lib:printable_unicode_list(S)).
+ ?SUCHTHAT(S, non_empty(string()), io_lib:printable_unicode_list(S)).
 
+unicode_string2() ->
+ ?SUCHTHAT(S, non_empty(utf8()), io_lib:printable_unicode_list(unicode:characters_to_list(S))).
+
+unicode_string3() ->
+ ?SUCHTHAT(S, ?LET(S1, non_empty(utf8()), unicode:characters_to_list(S1)) , io_lib:printable_unicode_list(S)).
